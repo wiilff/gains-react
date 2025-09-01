1b3b5d074c6d6e87443e5270f83dc9ebe5a070c7
wiilff <mattiswulff@gmail.com>
2025-09-01 16:28:20 +0200

    v1

--- src/api/exercises.js
+++ src/api/exercises.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
 
 export const getExercises = async () => {
   const res = await fetch(`${API_URL}/api/exercise`);
